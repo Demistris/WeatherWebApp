2dc07f8a1abf2c24c3f6262525826340c8b705b7
Kaja Więckowska <k.wieckowska66@gmail.com>
2024-09-11 17:05:11 +0200

    Removing data

--- WeatherApp/WeatherApp/AppConstants.cs
+++ WeatherApp/WeatherApp/AppConstants.cs
@@ -3,9 +3,9 @@
     public static class AppConstants
     {
         public static readonly string WeatherApiUrl = "http://api.openweathermap.org/data/2.5";
-        public static readonly string WeatherApiKey = "3959591d9fb94c55e722b5103e81620e";
+        public static readonly string WeatherApiKey = "";
 
         public static readonly string MapApiUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places";
-        public static readonly string MapApiKey = "pk.eyJ1IjoiZGVtaXN0cmlzIiwiYSI6ImNsd3draTdxbTEyczkybnIxOTl3aWhuajcifQ.k89FH9wxZQiVB0rTF3n6bQ";
+        public static readonly string MapApiKey = "";
     }
 }
